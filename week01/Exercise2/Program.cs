ab3d64b881aa8780c95457e9a495e88a3bac5153
noordak <kevnoorda@gmail.com>
2025-09-07 19:38:47 -0600

    added codes and comments

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,5 +5,42 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        string answer = Console.ReadLine();
+        int percent = int.Parse(answer);
+
+        string letter = "";
+
+        if (percent >= 90)
+        {
+            letter = "A: Excellent";
+        }
+        else if (percent >= 80)
+        {
+            letter = "B: Good";
+        }
+        else if (percent >= 70)
+        {
+            letter = "C: Average";
+        }
+        else if (percent >= 60)
+        {
+            letter = "D: Poor";
+        }
+        else
+        {
+            letter = "F: Sorry friend, you failed";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if (percent >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Try again and never give up");
+        }
     }
 }
